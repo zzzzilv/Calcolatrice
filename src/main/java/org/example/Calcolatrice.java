7f814911ef7769da30db30496c9afb7f617fdb17
andrea <a.gisolfi4@studenti.unisa.it>
2023-10-01 10:12:28 +0200

    Aggiunto il metodo potenza

--- src/main/java/org/example/Calcolatrice.java
+++ src/main/java/org/example/Calcolatrice.java
@@ -16,5 +16,7 @@ public class Calcolatrice {
     public double sottrazione(double a, double b) { return a - b;}
     public double divisione(double a, double b){ if(b != 0) {return a / b;} else return 0;}
 
+    public double potenza (double a, double b){ return Math.pow(a, b);}
+
     private double a, b;
 }
